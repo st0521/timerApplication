128747800bd539b92c49828b1d63c051624eec93
st0521 <blue-h@pg8.so-net.ne.jp>
2022-08-27 14:15:00 +0900

    ファイル追加

---
+++ timer.js
@@ -0,0 +1,145 @@
+const timerElement = document.getElementById('thetimer');
+const timeValue = timeElement.innerText;
+console.log(timeValue);
+const val = '0000'
+const startBtn = document.getElementById('startBtn');
+let timeoutId;
+startBtn.addEventListener('click', () => {
+   timeoutID = setTimeout(()=> {console.log("timeout")}, 5*1000);
+});
+const stopBtn = document.getElementById('stopBtn');
+stopBtn.addEventListener('click', () => {
+  clearTimeout(timeoutID);
+  console.log("timer cancel");
+});
+
+function btnOneClick() {
+    target = document.getElementById('the timer');
+    const base = target.innerText.replace(':','');
+    const newDisplay = base.substr(1,3)+ '1';
+   
+    const a = newDisplay.slice(0,2);
+    const b = ':';
+    const c = newDisplay.slice(2);
+    let nnDisplay = a + b + c;
+    console.log(nnDisplay);
+    document.getElementById('the timer').textContent = nnDisplay;
+    target.innerText = nnDisplay;
+  };
+  function btnTwoClick() {
+    target = document.getElementById('the timer');
+    const base = target.innerText.replace(':','');
+    const newDisplay = base.substr(1,3)+ '2';
+   
+    const a = newDisplay.slice(0,2);
+    const b = ':';
+    const c = newDisplay.slice(2);
+    let nnDisplay = a + b + c;
+    console.log(nnDisplay);
+    document.getElementById('the timer').textContent = nnDisplay;
+    target.innerText = nnDisplay;
+  };
+  function btnThreeClick() {
+    target = document.getElementById('the timer');
+    const base = target.innerText.replace(':','');
+    const newDisplay = base.substr(1,3)+ '3';
+   
+    const a = newDisplay.slice(0,2);
+    const b = ':';
+    const c = newDisplay.slice(2);
+    let nnDisplay = a + b + c;
+    console.log(nnDisplay);
+    document.getElementById('the timer').textContent = nnDisplay;
+    target.innerText = nnDisplay;
+  };
+  function btnFourClick() {
+    target = document.getElementById('the timer');
+    const base = target.innerText.replace(':','');
+    const newDisplay = base.substr(1,3)+ '4';
+   
+    const a = newDisplay.slice(0,2);
+    const b = ':';
+    const c = newDisplay.slice(2);
+    let nnDisplay = a + b + c;
+    console.log(nnDisplay);
+    document.getElementById('the timer').textContent = nnDisplay;
+    target.innerText = nnDisplay;
+  };
+  function btnFiveClick() {
+    target = document.getElementById('the timer');
+    const base = target.innerText.replace(':','');
+    const newDisplay = base.substr(1,3)+ '5';
+   
+    const a = newDisplay.slice(0,2);
+    const b = ':';
+    const c = newDisplay.slice(2);
+    let nnDisplay = a + b + c;
+    console.log(nnDisplay);
+    document.getElementById('the timer').textContent = nnDisplay;
+    target.innerText = nnDisplay;
+  };
+  function btnSixClick() {
+    target = document.getElementById('the timer');
+    const base = target.innerText.replace(':','');
+    const newDisplay = base.substr(1,3)+ '6';
+   
+    const a = newDisplay.slice(0,2);
+    const b = ':';
+    const c = newDisplay.slice(2);
+    let nnDisplay = a + b + c;
+    console.log(nnDisplay);
+    document.getElementById('the timer').textContent = nnDisplay;
+    target.innerText = nnDisplay;
+  };
+  function btnSevenClick() {
+    target = document.getElementById('the timer');
+    const base = target.innerText.replace(':','');
+    const newDisplay = base.substr(1,3)+ '7';
+   
+    const a = newDisplay.slice(0,2);
+    const b = ':';
+    const c = newDisplay.slice(2);
+    let nnDisplay = a + b + c;
+    console.log(nnDisplay);
+    document.getElementById('the timer').textContent = nnDisplay;
+    target.innerText = nnDisplay;
+  };
+  function btnEightClick() {
+    target = document.getElementById('the timer');
+    const base = target.innerText.replace(':','');
+    const newDisplay = base.substr(1,3)+ '8';
+   
+    const a = newDisplay.slice(0,2);
+    const b = ':';
+    const c = newDisplay.slice(2);
+    let nnDisplay = a + b + c;
+    console.log(nnDisplay);
+    document.getElementById('the timer').textContent = nnDisplay;
+    target.innerText = nnDisplay;
+  };
+  function btnNineClick() {
+    target = document.getElementById('the timer');
+    const base = target.innerText.replace(':','');
+    const newDisplay = base.substr(1,3)+ '9';
+   
+    const a = newDisplay.slice(0,2);
+    const b = ':';
+    const c = newDisplay.slice(2);
+    let nnDisplay = a + b + c;
+    console.log(nnDisplay);
+    document.getElementById('the timer').textContent = nnDisplay;
+    target.innerText = nnDisplay;
+  };
+  function btnZeroClick() {
+    target = document.getElementById('the timer');
+    const base = target.innerText.replace(':','');
+    const newDisplay = base.substr(1,3)+ '0';
+   
+    const a = newDisplay.slice(0,2);
+    const b = ':';
+    const c = newDisplay.slice(2);
+    let nnDisplay = a + b + c;
+    console.log(nnDisplay);
+    document.getElementById('the timer').textContent = nnDisplay;
+    target.innerText = nnDisplay;
+  };
